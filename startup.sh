3c93f37c6b7fcfe26ed03ac34dae780e95b46876
Caio Kormives <caio.kormives@ti.hubfloripa.com.br>
2026-03-31 23:40:45 -0300

    ci: use --build-remote true (Oryx), fix startup, 1 worker

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export PYTHONPATH="${PYTHONPATH}:/home/site/wwwroot/.packages"
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 --timeout 120
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 --timeout 120
